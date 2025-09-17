6df3f999ad238bb1679a056db154b622a1600e24
Lucas Zhao <lucas.zhao@yale.edu>
2025-09-17 11:54:57 -0400

    added speak.c

---
+++ speak.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("H\n");
+    printf("e\n");
+    printf("l\n");
+    printf("l\n");
+    printf("o\n");
+    printf("o\n"); 
+    printf("!\n");
+    return 0;
+}
