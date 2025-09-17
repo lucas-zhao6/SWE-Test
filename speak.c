d452be40f9a71a74fd63575294174304fff14ff8
Lucas Zhao <lucas.zhao@yale.edu>
2025-09-17 12:01:06 -0400

    modified speak.c

--- speak.c
+++ speak.c
@@ -7,6 +7,12 @@ int main() {
     printf("l\n");
     printf("o\n");
     printf("o\n"); 
+    printf(" \n");
+    printf("t\n");
+    printf("h\n");
+    printf("e\n");
+    printf("r\n");
+    printf("e\n");
     printf("!\n");
     return 0;
 }
